85bfafc8a077a699f81ec242c443606163621957
teslmik <teslmik@gmail.com>
2023-04-29 13:55:59 +0300

    add user & files CRUD

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
 
   app.enableCors({ credentials: true, origin: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cloud Storage')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(4000);
 }
