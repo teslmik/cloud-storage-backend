fbeca54d7041564cacc9ebef1b5751e68097cafe
teslmik <teslmik@gmail.com>
2023-05-17 13:18:58 +0300

    fix

--- src/decorators/user-id.decorator.ts
+++ src/decorators/user-id.decorator.ts
@@ -3,7 +3,8 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const UserId = createParamDecorator(
   (_: unknown, ctx: ExecutionContext): number | null => {
     const request = ctx.switchToHttp().getRequest();
+    const { id } = request.user;
 
-    return request.user?.id ? Number(request.user.id) : null;
+    return id ? Number(id) : null;
   },
 );
